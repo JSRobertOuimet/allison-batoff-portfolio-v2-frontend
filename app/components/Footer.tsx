fd19136183eb634ec9e7f07b6747c0db2a4f734f
JSRobertOuimet <jsrobertouimet@gmail.com>
2025-09-07 19:53:27 -0400

    Updated styles.

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <nav className="sticky top-[100vh] container mx-auto p-4 flex justify-center">
+        <nav className="sticky top-[100vh] container mx-auto p-4 flex justify-center text-gray-500">
             &copy; Allison Batoff {new Date().getFullYear()}
         </nav>
     );
