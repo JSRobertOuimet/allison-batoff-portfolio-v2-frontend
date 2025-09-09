4921e5fdf83d79844ad529bb15a845ab9eaa5ab7
JSRobertOuimet <jsrobertouimet@gmail.com>
2025-09-09 17:34:11 -0400

    Updated layout reflow.

--- app/components/CaseStudyTile.tsx
+++ app/components/CaseStudyTile.tsx
@@ -3,19 +3,29 @@ import { Link } from "react-router";
 
 const CaseStudyTile = ({ caseStudy }: { caseStudy: CaseStudyMeta }) => {
     return (
-        <Link to={`/design/${caseStudy.slug}`} className="p-4 flex gap-8 outline-transparent hover:outline hover:outline-gray-300 focus:outline focus:outline-gray-300 transition duration-500">
+        <Link
+            to={`/design/${caseStudy.slug}`}
+            className="flex flex-col md:flex-row p-4 gap-8 outline-transparent hover:outline hover:outline-gray-300 focus:outline focus:outline-gray-300 transition duration-500 group">
             <img
                 src={caseStudy.thumbnail.imageUrl}
                 alt={caseStudy.thumbnail.alternativeText}
-                className="w-1/3 aspect-square object-cover"
+                className="md:w-1/3 aspect-square object-cover"
             />
             <div>
                 <h2 className="text-2xl mb-4">{caseStudy.title}</h2>
-                <p className="mb-6">{caseStudy.description}</p>
-                {caseStudy.categories.map(category => (
-                    <span key={category.category} className="mr-4 px-2 py-1 rounded-full border-1 border-gray-300 text-sm">
+                <p className="mb-6 text-gray-500">
+                    {caseStudy.description}
+                </p>
+                <div className="group"></div>
+                {caseStudy.categories.map((category, i) => (
+                    <div
+                        key={category.category}
+                        style={
+                            { ["--i" as any]: i } as React.CSSProperties
+                        }
+                        className="inline-block mr-2 mb-2 px-2 py-1 rounded-full border-1 border-gray-300 text-sm opacity-0 transition-all duration-300 delay-[calc(var(--i)*90ms)] group-hover:opacity-100 group-focus:opacity-100">
                         {category.category}
-                    </span>
+                    </div>
                 ))}
             </div>
         </Link>
