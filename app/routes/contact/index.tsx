aa6341b06a92183dcd392fa108f06a7ad180305e
JSRobertOuimet <jsrobertouimet@gmail.com>
2025-09-09 09:06:19 -0400

    Removed rounded corners to button.

--- app/routes/contact/index.tsx
+++ app/routes/contact/index.tsx
@@ -110,7 +110,7 @@ export default function ContactPage() {
                             required
                         />
                     </div>
-                    <button className="bg-gray-950 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded cursor-pointer">
+                    <button className="bg-gray-950 hover:bg-gray-800 text-white font-bold py-2 px-4 cursor-pointer">
                         Submit
                     </button>
                 </form>
